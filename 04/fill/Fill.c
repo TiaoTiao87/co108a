aee8d5291ae50a92f830e02f9d1b7e4ff6f81668
TiaoTiao87 <47874872+TiaoTiao87@users.noreply.github.com>
2019-12-18 17:01:04 +0800

    Create Fill.c

---
+++ 04/fill/Fill.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    arr = SCREEN;
+    n = 8192;
+    i = 0;
+    while(i<n)
+    {
+        if(*KBD != 0)
+        arr[i] = -1 //black
+        i=i+1
+        else
+        arr[i] = 0 //white
+    }
+}
